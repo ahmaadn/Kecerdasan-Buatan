1dc504584b4bd80fb986d32ebe853a3b8d7873ec
Ahmad Nur <62164035+ahmaadn@users.noreply.github.com>
2024-03-04 20:09:58 +0700

    feet: set min x dan y

--- Praktikum/L2/L2_RandomSearch.py
+++ Praktikum/L2/L2_RandomSearch.py
@@ -1,9 +1,11 @@
 import random
-import copy
 
-def get_input(label):
+def get_input(label, min_num):
     try:
-        return int(input(f"Goal untuk {label}, default ('n')? "))
+        result =  int(input(f"Goal untuk {label}, default ('n')? "))
+        if result >= 0 and result <= min_num:
+            return result
+        return get_input(label, min_num)
     except ValueError:
         return 'n'
 
@@ -11,8 +13,8 @@ def is_valid(state, goal):
     x, y = state
     gx, gy = goal
 
-    if ((not isinstance(gx, int) and y == gy) 
-        or (gx == x and not isinstance(gy, int)) 
+    if ((not isinstance(gx, int) and (y == gy))
+        or ((x == gx) and not isinstance(gy, int))
         or ((x, y) == (gx, gy))
         or (not isinstance(gx, int) and not isinstance(gy, int))
     ):
@@ -22,10 +24,10 @@ def is_valid(state, goal):
 
 def recursive_random_search(initial, goal, n=0):
     if is_valid(initial, goal):
-            return initial
+        return initial
 
     func_rule = random.choice(rules)
-    proir_initial = copy.copy(initial)
+    proir_initial = initial[:]
     index_rule = rules.index(func_rule) + 1
     initial = func_rule(*initial)
 
@@ -36,14 +38,15 @@ def recursive_random_search(initial, goal, n=0):
 def random_search(initial, goal):
     if is_valid(initial, goal):
         return initial
-    n=0
+
+    n = 0
     while True:
         func_rule = random.choice(rules)
-        proir_initial = copy.copy(initial)
+        proir_initial = initial[:]
         index_rule = rules.index(func_rule) + 1
         initial = func_rule(*initial)
         print(f"{n}: Aturan ke-{index_rule} : {proir_initial} -> {initial}")
-        
+
         if is_valid(initial, goal):
             return initial
         n += 1
@@ -59,7 +62,9 @@ rules = [
     lambda x, y : (0, y + x)  if x + y <= 3 and x > 0 else (x, y), # 8
 ]
 
-gx = get_input("x")
-gy = get_input("y")
+min_x = 4
+max_y = 3
+gx = get_input("x", min_x)
+gy = get_input("y", max_y)
 x, y = random_search((0, 0), (gx, gy))
 print(f"Akhir: {(x, y)}")
