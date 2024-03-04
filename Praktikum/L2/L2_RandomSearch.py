f7f14475b463951cece20fd344b5ab75226ba7fc
Ahmad Nur <62164035+ahmaadn@users.noreply.github.com>
2024-03-04 16:02:27 +0700

    add: menambahkan L2 random search

---
+++ Praktikum/L2/L2_RandomSearch.py
@@ -0,0 +1,49 @@
+import random
+import copy
+
+def get_input(label):
+    try:
+        return int(input(f"Goal untuk {label} : default ('n')? "))
+    except ValueError:
+        return 'n'
+
+def is_valid(state, goal):
+    x, y = state
+    gx, gy = goal
+
+    if ((not isinstance(gx, int) and y == gy) 
+        or (gx == x and not isinstance(gy, int)) 
+        or ((x, y) == (gx, gy))
+    ):
+        return True
+
+    return False
+
+def recursice_random_search(initial, goal, n=0):
+    if is_valid(initial, goal):
+            return initial
+    
+    func_rule = random.choice(rules)
+    proir_initial = copy.copy(initial)
+    index_rule = rules.index(func_rule) + 1
+    initial = func_rule(*initial)
+    
+    print(f"Aturan ke-{index_rule} : {proir_initial} -> {initial}")
+    
+    return recursice_random_search(initial, goal, n+1)
+
+
+rules = [
+    lambda x, y : (4, y) if x < x else (x, y), # 1
+    lambda x, y : (x, 3) if y < 3 else (x, y), # 2
+    lambda x, y : (0, y) if x > 0 else (x, y), # 3
+    lambda x, y : (0, y) if y > 0 else (x, y), # 4
+    lambda x, y : (4, y - (4-x)) if x + y >= 4 and y > 0 else (x, y), # 5
+    lambda x, y : (x - (3-y), 3) if x + y >= 3 and x > 0 else (x, y), # 6
+    lambda x, y : (x + y, 0) if x + y <= 4 and y > 0 else (x, y), # 7
+    lambda x, y : (0, y + x)  if x + y <= 3 and x > 0 else (x, y), # 8
+]
+
+gx = get_input("x")
+gy = get_input("y")
+x, y = recursice_random_search((0, 0), (gx, gy))
