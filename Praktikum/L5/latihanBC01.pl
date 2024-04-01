dbdc92734811330e57b68405b59182bb4d417583
yoga220802 <yogaagustiansyah01@gmail.com>
2024-04-01 14:37:24 +0700

    feet: add FC2

--- Praktikum/L5/latihanBC01.pl
+++ Praktikum/L5/latihanBC01.pl
@@ -1,29 +1,38 @@
-% fakta
-orangtua(ahmad, budi).
-orangtua(ahmad, candra).
-orangtua(budi, cindy).
-orangtua(budi, david).
-orangtua(candra, emily).
-orangtua(candra, fahri).
-orangtua(david, grace).
+saudara(john, mary).
+saudara(john, paul).
+saudara(mary, peter).
+saudara(peter, anne).
+orang_tua(bob, john).
+orang_tua(bob, mary).
+orang_tua(bob, paul).
+orang_tua(mary_senior, mary).
+orang_tua(mary_senior, paul).
 
-% Aturan
-kakek(Kakek, Cucu) :-
-    orangtua(Kakek, Anak),
-    orangtua(Anak, Cucu).
-
-saudara(Saudara1, Saudara2) :-
-    orangtua(Orangtua, Saudara1),
-    orangtua(Orangtua, Saudara2),
-    Saudara1 \== Saudara2.
-
-paman(Paman, Keponakan) :-
-    saudara(Paman, Orangtua),
-    saudara(Orangtua, Keponakan).
-
-cucu(Cucu, Kakek) :-
-    kakek(Kakek, Cucu).
-cucu(Cucu, Kakek) :-
-    orangtua(Orangtua, Cucu),
-    cucu(Orangtua, Kakek).
-
+/aturan/
+anak(X,Y):-
+    orang_tua(Y,X).
+keponakan(X,Y):-
+    anak(X,Z), saudara(Z,Y).
+cucu(X,Y):-
+    anak(X,Z), orang_tua(Z,Y).
+paman(X,Y):-
+    saudara(X,Z), orang_tua(Z,y).
+penalaran(X,Y):-
+    write('masukan nama orang:'),
+    read(nama),tulis_hubungan(nama),!.
+tulis_hubungan(nama):-
+    anak(nama, orangtua),
+    write(nama),write('adalah anak dari'),write(orangtua),nl,
+    fail.
+tulis_hubungan(nama):-
+    anak(nama, paman),
+    write(nama),write('adalah keponakan dari'),write(orangtua),nl,
+    fail.
+tulis_hubungan(nama):-
+    anak(nama, kakek),
+    write(nama),write('adalah cucu dari'),write(orangtua),nl,
+    fail.
+tulis_hubungan(nama):-
+    anak(nama, orang),
+    write(nama),write('adalah paman dari'),write(orangtua),nl,
+    fail.
